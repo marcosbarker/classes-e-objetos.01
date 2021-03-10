5aff4f53a99f5598c69be8a6fb7763d932cfee3c
Marcos Paulo Marques Corrêa <marcos_barker@hotmail.com>
2021-03-10 16:17:41 -0300

    first commit

---
+++ classe_carta.h
@@ -0,0 +1,9 @@
+class carta: private figura{
+	std::string naipe;
+	
+public:
+	std::string getfigura ();
+	void setfigura (std::string umfigura);
+	void getnaipe ();
+	void setnaipe (std::string umnaipe);
+};
